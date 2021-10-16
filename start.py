e1362596bb0780d10d370790fcbc27691beb68da
Paul <paulsolin@gmail.com>
2021-10-16 10:08:55 -0400

    Code refactoring

--- start.py
+++ start.py
@@ -1,37 +1,33 @@
 from flask import Flask, render_template, Response
 from random import randint
-import time
 import json
 
 app = Flask(__name__)
 
+
 @app.route("/data")
-def chart_data(data=None):
+def chart_data():
+    data_set = []
 
-	data_set = []
+    for x in range(0, 12):
+        y = randint(1, 12)
+        data_set.append(y)
 
-	for x in range(0,12):
-		y = randint(1, 12)
-		data_set.append(y)
+    data = {'set': data_set}
 
-	data = {}
+    js = json.dumps(data)
 
-	data['set'] = data_set
+    resp = Response(js, status=200, mimetype='application/json')
 
-	js = json.dumps(data)
+    return resp
 
-	resp = Response(js, status=200, mimetype='application/json')
-
-	return resp
 
 @app.route("/")
-def hello(data=None):
+def hello():
+    data = {'title': 'Chart'}
 
-	data = {}
+    return render_template('index.html', data=data)
 
-	data['title'] = 'Chart'
-
-	return render_template('index.html', data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
